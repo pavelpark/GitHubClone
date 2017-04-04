2ae07eca6f527120a7add5f94338c2747d5d33ff
Pavel Parkhomey <pavelpark@aol.com>
2017-04-04 11:15:58 -0700

    Created a new file Repository.

---
+++ GitHubClone/GitHubClone/Repository.swift
@@ -0,0 +1,25 @@
+//
+//  Repository.swift
+//  GitHubClone
+//
+//  Created by Pavel Parkhomey on 4/4/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+import Foundation
+
+class Repository {
+    
+    let name: String
+    let description: String?
+    let language: String?
+    
+    init?(json: [String: Any]){
+        print("Complete This for Lab")
+        
+        print(json)
+        
+        return nil
+        
+    }
+}
